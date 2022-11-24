c6f7aab797a5e4ce4782f232e44fa5c87ce6f32e
Yuri Edward <yuri6037@outlook.com>
2022-11-24 21:08:30 +0100

    Added CSVParser

--- TextTools/CSVParser.m
+++ TextTools/CSVParser.m
@@ -1,6 +1,6 @@
 // Copyright 2022 Yuri6037
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
+// Permission is hereby granted, free of charge, to any person obtaining a 
 // copy
 // of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
@@ -13,21 +13,16 @@
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
 // THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 // DEALINGS
 // IN THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
+#import "CSVParser.h"
 
-//! Project version number for TextTools.
-FOUNDATION_EXPORT double TextToolsVersionNumber;
+@implementation CSVParser
 
-//! Project version string for TextTools.
-FOUNDATION_EXPORT const unsigned char TextToolsVersionString[];
-
-#import <TextTools/BufferedTextFile.h>
-#import <TextTools/CSVParser.h>
+@end
