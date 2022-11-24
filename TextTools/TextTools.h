ce4c0f429ebf8538ceb824d7aebfbff0fbb8afc6
Yuri Edward <yuri6037@outlook.com>
2022-11-24 20:56:12 +0100

    Initial Commit

---
+++ TextTools/TextTools.h
@@ -0,0 +1,18 @@
+//
+//  TextTools.h
+//  TextTools
+//
+//  Created by Yuri Edward on 11/24/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TextTools.
+FOUNDATION_EXPORT double TextToolsVersionNumber;
+
+//! Project version string for TextTools.
+FOUNDATION_EXPORT const unsigned char TextToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextTools/PublicHeader.h>
+
+
